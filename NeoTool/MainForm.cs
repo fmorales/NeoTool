3fac3fcd373e28bad51e45ee1e73f1ea91a70793
lempamo <smartypantslf@gmail.com>
2018-10-16 17:01:49 -0400

    valid account detection

--- NeoTool/MainForm.cs
+++ NeoTool/MainForm.cs
@@ -21,7 +21,10 @@ namespace NeoTool {
             InitializeComponent();
 
             // Init/load settings
-            if (Settings.Default.Accounts == null | Settings.Default.Accounts.Count == 0) {
+            if (Settings.Default.Accounts == null) {
+                Settings.Default.Accounts = new List<AccountData>();
+                AddAccount();
+            } else if (Settings.Default.Accounts.Count == 0) {
                 Settings.Default.Accounts = new List<AccountData>();
                 AddAccount();
             }
@@ -43,21 +46,17 @@ namespace NeoTool {
                 client.Authenticator = new HttpBasicAuthenticator(login.kryptonTextBox1.Text, login.kryptonTextBox2.Text);
 
                 var request = new RestRequest("key", Method.GET, DataFormat.Json);
-                IRestResponse response = null;
-                bool isGoing = true;
-                client.ExecuteAsync(request, r => {
-                    response = r;
-                    isGoing = false;
-                });
 
-                while (isGoing) { }
-
-                dynamic data = JsonConvert.DeserializeObject(response.Content);
-                if (data.result == "error")
+                dynamic data = JsonConvert.DeserializeObject(client.Execute(request).Content);
+                if (data.result == "error") {
                     if (data.error_type == "invalid_auth") {
                         MessageBox.Show(login, "Invalid username and/or password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                         continue;
                     }
+                } else {
+                    Settings.Default.Accounts.Add(new AccountData(login.kryptonTextBox1.Text, login.kryptonTextBox2.Text));
+                    break;
+                }
             }
             Settings.Default.Save();
         }
